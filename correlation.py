adadb8a0a25b1eecaade744fe34a20efb9420f44
root <ivarknut@server>
2018-05-16 15:00:47 +0200

    correlation script working

--- correlation.py
+++ correlation.py
@@ -1,7 +1,11 @@
-from pyvg import Graph, Path
+from pyvg import Graph, Path, Alignment
 from pyvg.conversion import get_json_paths_from_json
 import offsetbasedgraph as obg
 import numpy as np
+import json
+
+def get_alignments(filename):
+    return (json.loads(line) for line in open(filename))
 
 
 def get_path_nodes(graph):
@@ -54,26 +58,32 @@ def get_sequence(sequence_graph, node_ids):
 
 def main(alignments_file_name, corr_file_name):
     corr_struct = np.load(corr_file_name)
-    paths = get_json_paths_from_json(alignments_file_name)
-    paths = (Path.from_json(path) for path in paths)
-    corrs = (get_correlations(path, corr_struct) for path in paths)
+    paths = get_alignments(alignments_file_name)
+    paths = [Alignment.from_json(path) for path in paths]
+    print(len(paths))
+    corrs = {path.name: get_correlations(path.path, corr_struct) for path in paths}
+    print(len(corrs))
     cidra_graph = obg.Graph.from_file(data_folder + "cidra.nobg")
     cidra_sequence_graph = obg.SequenceGraph.from_file(data_folder + "cidra.nobg.sequences")
-    predicted = (predict_path(corr, cidra_graph)
-                 for corr in corrs)
-    return (get_sequence(cidra_sequence_graph, pred) for pred in predicted)
+    predicted = {name: predict_path(corr, cidra_graph)
+                 for name, corr in corrs.items()}
+    return {name: get_sequence(cidra_sequence_graph, pred) for name, pred in predicted.items()}
 
 if __name__ == "__main__":
+    import sys
     data_folder = "../../data/malaria/pfemp_sequences/150genes/"
     # corr = get_correlation(data_folder+"dbla.json", data_folder+"cidra.json")
     # np.save(data_folder + "dbla_cidra_corr.npy", corr)
     # alignment_corr = get_correlations(data_folder+"alignments.json", data_folder+"dbla_cidra_corr.npy")
     # np.save(data_folder + "tmp_align_corr.npy", alignment_corr)
-    res = main(data_folder + "alignments.json",
+    res = main(sys.argv[1],
                data_folder + "dbla_cidra_corr.npy")
-    for r in res:
-        print(r)
-        print("****************")
+
+    with open(sys.argv[1] + "graph_predictions.fasta", "w") as f:
+        for name, r in res.items():
+            f.write("> %s\n" % name)
+            f.write(r+"\n")
+
     # 
     # path = predict_path(np.load(data_folder + "tmp_align_corr.npy"), 
     #                     obg.Graph.from_file(data_folder + "cidra.nobg"))
