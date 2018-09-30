0c01f6c05fffeb18466780b8cb47cbf3650a1bbc
Knut Dagestad Rand <knutdrand@gmail.com>
2018-09-30 11:13:39 +0200

    Update predict_linear.sh
    Comment out blast bug

--- predict_linear.sh
+++ predict_linear.sh
@@ -1,6 +1,8 @@
 # Running: ./predict_linear.sh /home/ivar/data/malaria/pfemp_sequences/150genes/dbla_test.fasta /home/ivar/data/malaria/pfemp_sequences/150genes/dbla.fasta /home/ivar/data/malaria/pfemp_sequences/150genes/cidra.fasta > linear_predictions.fasta
 
-blastn -query $1 -db $2 -outfmt 6 -max_target_seqs 1 > blast_res.tmp 
+echo "TODO: Fix blastn bug"
+# blastn -query $1 -db $2 -outfmt 6 -max_target_seqs 1 > blast_res.tmp 
+exit(-1)
 python3 parse_blast_output.py blast_res.tmp $3
 
 #cut -f1,2 blast_res.tmp > original_to_matched_ids.tmp
